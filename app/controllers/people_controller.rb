667071c47fb223bac56df7796f61684290e418e8
99chris99 <chrishodge87@gmail.com>
2020-03-04 15:05:21 +0000

    completed people_controller

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,2 +1,30 @@
 class PeopleController < ApplicationController
+
+def index
+    people = Person.all
+    render json: people
+end    
+
+def show
+    person = Person.find_by(name: params[:name])
+    render json: person
+end    
+
+def create
+    person = Person.all.new(name: params[:name])
+    if person.save
+        render json: person
+    end
 end
+
+
+  
+    
+
+
+    
+
+
+
+    
+end #class end
