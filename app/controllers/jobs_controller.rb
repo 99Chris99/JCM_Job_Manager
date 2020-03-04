6c3a5666aa8488ec4d0b9a32f99691114251b027
99chris99 <chrishodge87@gmail.com>
2020-03-04 14:26:37 +0000

    completed jobs_controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,45 @@
 class JobsController < ApplicationController
+
+def index
+    jobs = Job.All 
+    render json: jobs
 end
+
+def show
+    job = Job.find_by(id:params[:id])
+end
+
+def create
+    job = Job.new(name: params[:name], status: params[:status], start_date: params[:start_date], finish_date: params[:finish_date], extid: params[:extid], notes: notes[:notes])
+        if job.save
+            render json: job
+        else
+            render json: {errors: job.errors.full_message }, status: 403
+        end
+end
+
+def update
+    job = Job.find(params[:id])
+    if job.update(job: params[:job])
+        render json: job
+    end
+end
+
+
+def destroy
+    job = Job.find_by(id:params[:id])
+    if job
+        job.destroy
+        render json: job
+    end
+end
+
+private 
+
+def job_params
+    params.require(:job.permit(:name, :status, :start_date, :finish_date, :extid, :notes))
+end
+
+
+end #class end
+
